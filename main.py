11a60ae130a499f8975dbcf5d61320e3b5357338
TeleportYT <vivien110705@gmail.com>
2024-05-02 16:47:11 +0300

    Added main script to run added start message and a function that prints the game progress by the amount of guess that left

---
+++ main.py
@@ -0,0 +1,56 @@
+import random
+
+NUM_OF_GUESS = 6 ;'''random.randint(5, 11)'''
+GAME_PARTS = ["""x-------x""","""    x-------x
+    |
+    |
+    |
+    |
+    |
+""","""    x-------x
+    |       |
+    |       0
+    |
+    |
+    |""","""    x-------x
+    |       |
+    |       0
+    |       |
+    |
+    |
+""","""    x-------x
+    |       |
+    |       0
+    |      /|\\
+    |
+    |""","""    x-------x
+    |       |
+    |       0
+    |      /|\
+    |      /
+    |
+""","""    x-------x
+    |       |
+    |       0
+    |      /|\
+    |      / \
+    |"""]
+def printStatus():
+    print(GAME_PARTS[NUM_OF_GUESS-6])
+
+
+def main():
+    print("Welcome to the game Hangman")
+    print("""    _    _
+   | |  | |
+   | |__| | __ _ _ __   __ _ _ __ ___   __ _ _ __
+   |  __  |/ _' | '_ \ / _' | '_ ' _ \ / _' | '_ \\
+   | |  | | (_| | | | | (_| | | | | | | (_| | | | |
+   |_|  |_|\__,_|_| |_|\__, |_| |_| |_|\__,_|_| |_|
+                        __/ |
+                       |___/""")
+    print(f"You have {NUM_OF_GUESS} guesses")
+    printStatus()
+
+if __name__ == "__main__":
+    main()
