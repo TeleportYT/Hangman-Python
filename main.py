219953e3788be13b2036a098dc07e92ff121b60f
TeleportYT <vivien110705@gmail.com>
2024-05-02 16:50:06 +0300

    Exercise 2.5.2, added asking for letter

--- main.py
+++ main.py
@@ -39,6 +39,7 @@ def printStatus():
     print(GAME_PARTS[NUM_OF_GUESS-6])
 
 
+
 def main():
     print("Welcome to the game Hangman")
     print("""    _    _
@@ -50,7 +51,9 @@ def main():
                         __/ |
                        |___/""")
     print(f"You have {NUM_OF_GUESS} guesses")
-    printStatus()
+
+    letter = input("Guess a letter ")
+    print(letter)
 
 if __name__ == "__main__":
     main()
